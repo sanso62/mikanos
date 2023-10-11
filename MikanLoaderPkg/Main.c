790b37e6eb9ea0e2a5957096df50d8f1d4a8e554
sanso62 <jmd9762@gmail.com>
2023-10-11 20:02:11 +0900

    osbook_day04b 커널 자유자재로 그리기

--- MikanLoaderPkg/Main.c
+++ MikanLoaderPkg/Main.c
@@ -8,8 +8,9 @@
 #include  <Protocol/DiskIo2.h>
 #include  <Protocol/BlockIo.h>
 #include  <Guid/FileInfo.h>
+#include  "frame_buffer_config.hpp"
 
-struct MemoryMap{
+struct MemoryMap {
   UINTN buffer_size;
   VOID* buffer;
   UINTN map_size;
@@ -18,24 +19,18 @@ struct MemoryMap{
   UINT32 descriptor_version;
 };
 
-// 메모리 맵 취득
-EFI_STATUS GetMemoryMap(struct MemoryMap* map){
-  // 메모리 영역이 작아 메모리 맵을 전부 담기가 불가능한 경우
-  if (map->buffer == NULL){
+EFI_STATUS GetMemoryMap(struct MemoryMap* map) {
+  if (map->buffer == NULL) {
     return EFI_BUFFER_TOO_SMALL;
   }
 
   map->map_size = map->buffer_size;
-
-  // gBS: 메모리 관리관련 기능이 포함된 부트 서비스 글로벌 변수
-  // 부트 서비스:  UEFI가 OS를 기동하기 위해 필요한 기능을 제공
   return gBS->GetMemoryMap(
-    &map->map_size,                       // 메모리 맵 크기
-    (EFI_MEMORY_DESCRIPTOR*)map->buffer,  // 메모리 맵 시작 포인터
-    &map->map_key,                        // 메모리 맵 식별자
-    &map->descriptor_size,                // 메모리 맵 각 행을 나타내는 메모리 디스크립터 크기
-    &map->descriptor_version              // 메모리 디스크립터 버전
-  );
+      &map->map_size,
+      (EFI_MEMORY_DESCRIPTOR*)map->buffer,
+      &map->map_key,
+      &map->descriptor_size,
+      &map->descriptor_version);
 }
 
 const CHAR16* GetMemoryTypeUnicode(EFI_MEMORY_TYPE type) {
@@ -60,84 +55,99 @@ const CHAR16* GetMemoryTypeUnicode(EFI_MEMORY_TYPE type) {
   }
 }
 
-// 메모리 맵 정보를 csv파일에 기록
-EFI_STATUS SaveMemoryMap(struct MemoryMap* map, EFI_FILE_PROTOCOL* file){
+EFI_STATUS SaveMemoryMap(struct MemoryMap* map, EFI_FILE_PROTOCOL* file) {
+  EFI_STATUS status;
   CHAR8 buf[256];
   UINTN len;
 
-  CHAR8* header = 
-      "Index, Type, Type(name), PhysicalStart, NumberOfPages, Attribute\n";
+  CHAR8* header =
+    "Index, Type, Type(name), PhysicalStart, NumberOfPages, Attribute\n";
   len = AsciiStrLen(header);
-  file->Write(file, &len, header);
+  status = file->Write(file, &len, header);
+  if (EFI_ERROR(status)) {
+    return status;
+  }
 
   Print(L"map->buffer = %08lx, map->map_size = %08lx\n",
       map->buffer, map->map_size);
 
-  EFI_PHYSICAL_ADDRESS iter; // iter: 메모리 디스크립터 어드레스
-  int i; // i: 메모리 뱁의 행 번호를 나타내는 카운터
-  for(iter = (EFI_PHYSICAL_ADDRESS)map->buffer, i=0;
-      iter < (EFI_PHYSICAL_ADDRESS)map->buffer + map->map_size;
-      iter += map->descriptor_size, i++){
+  EFI_PHYSICAL_ADDRESS iter;
+  int i;
+  for (iter = (EFI_PHYSICAL_ADDRESS)map->buffer, i = 0;
+       iter < (EFI_PHYSICAL_ADDRESS)map->buffer + map->map_size;
+       iter += map->descriptor_size, i++) {
     EFI_MEMORY_DESCRIPTOR* desc = (EFI_MEMORY_DESCRIPTOR*)iter;
-
-    // AsciiPrint()는 EDK II에서 준비한 라이브러리 함수로, 지정한 char 배열에 가공한 문자열을 기록
     len = AsciiSPrint(
-      buf, sizeof(buf),
-      "%u, %x, %-ls, %08lx, %lx, %lx\n",
-      i, desc->Type, GetMemoryTypeUnicode(desc->Type),
-      desc->PhysicalStart, desc->NumberOfPages,
-      desc->Attribute & 0xffffflu);
-
-    // 파일에 문자열을 씀
-    file->Write(file, &len, buf);
+        buf, sizeof(buf),
+        "%u, %x, %-ls, %08lx, %lx, %lx\n",
+        i, desc->Type, GetMemoryTypeUnicode(desc->Type),
+        desc->PhysicalStart, desc->NumberOfPages,
+        desc->Attribute & 0xffffflu);
+    status = file->Write(file, &len, buf);
+    if (EFI_ERROR(status)) {
+      return status;
+    }
   }
 
   return EFI_SUCCESS;
 }
 
 EFI_STATUS OpenRootDir(EFI_HANDLE image_handle, EFI_FILE_PROTOCOL** root) {
+  EFI_STATUS status;
   EFI_LOADED_IMAGE_PROTOCOL* loaded_image;
   EFI_SIMPLE_FILE_SYSTEM_PROTOCOL* fs;
 
-  gBS->OpenProtocol(
+  status = gBS->OpenProtocol(
       image_handle,
       &gEfiLoadedImageProtocolGuid,
       (VOID**)&loaded_image,
       image_handle,
       NULL,
       EFI_OPEN_PROTOCOL_BY_HANDLE_PROTOCOL);
+  if (EFI_ERROR(status)) {
+    return status;
+  }
 
-  gBS->OpenProtocol(
+  status = gBS->OpenProtocol(
       loaded_image->DeviceHandle,
       &gEfiSimpleFileSystemProtocolGuid,
       (VOID**)&fs,
       image_handle,
       NULL,
       EFI_OPEN_PROTOCOL_BY_HANDLE_PROTOCOL);
+  if (EFI_ERROR(status)) {
+    return status;
+  }
 
-  fs->OpenVolume(fs, root);
-
-  return EFI_SUCCESS;
+  return fs->OpenVolume(fs, root);
 }
 
 EFI_STATUS OpenGOP(EFI_HANDLE image_handle,
                    EFI_GRAPHICS_OUTPUT_PROTOCOL** gop) {
+  EFI_STATUS status;
   UINTN num_gop_handles = 0;
   EFI_HANDLE* gop_handles = NULL;
-  gBS->LocateHandleBuffer(
+
+  status = gBS->LocateHandleBuffer(
       ByProtocol,
       &gEfiGraphicsOutputProtocolGuid,
       NULL,
       &num_gop_handles,
       &gop_handles);
+  if (EFI_ERROR(status)) {
+    return status;
+  }
 
-  gBS->OpenProtocol(
+  status = gBS->OpenProtocol(
       gop_handles[0],
       &gEfiGraphicsOutputProtocolGuid,
       (VOID**)gop,
       image_handle,
       NULL,
       EFI_OPEN_PROTOCOL_BY_HANDLE_PROTOCOL);
+  if (EFI_ERROR(status)) {
+    return status;
+  }
 
   FreePool(gop_handles);
 
@@ -167,97 +177,152 @@ void Halt(void) {
 
 EFI_STATUS EFIAPI UefiMain(
     EFI_HANDLE image_handle,
-    EFI_SYSTEM_TABLE *system_table) {
+    EFI_SYSTEM_TABLE* system_table) {
+  EFI_STATUS status;
+
   Print(L"Hello, Mikan World!\n");
-  
-  // 메모리 맵으로 사용할 변수 선언. 사이즈 16KiB
+
   CHAR8 memmap_buf[4096 * 4];
   struct MemoryMap memmap = {sizeof(memmap_buf), memmap_buf, 0, 0, 0, 0};
+  status = GetMemoryMap(&memmap);
+  if (EFI_ERROR(status)) {
+    Print(L"failed to get memory map: %r\n", status);
+    Halt();
+  }
 
-  // 메모리 맵 취득
-  GetMemoryMap(&memmap);
-
-  // 메모리 맵을 저장할 파일 쓰기 모드로 열기
   EFI_FILE_PROTOCOL* root_dir;
-  OpenRootDir(image_handle, &root_dir);
+  status = OpenRootDir(image_handle, &root_dir);
+  if (EFI_ERROR(status)) {
+    Print(L"failed to open root directory: %r\n", status);
+    Halt();
+  }
+
   EFI_FILE_PROTOCOL* memmap_file;
-  root_dir->Open(
-    root_dir, &memmap_file, L"\\memmap",
-    EFI_FILE_MODE_READ | EFI_FILE_MODE_WRITE | EFI_FILE_MODE_CREATE, 0
-  );
+  status = root_dir->Open(
+      root_dir, &memmap_file, L"\\memmap",
+      EFI_FILE_MODE_READ | EFI_FILE_MODE_WRITE | EFI_FILE_MODE_CREATE, 0);
+  if (EFI_ERROR(status)) {
+    Print(L"failed to open file '\\memmap': %r\n", status);
+    Print(L"Ignored.\n");
+  } else {
+    status = SaveMemoryMap(&memmap, memmap_file);
+    if (EFI_ERROR(status)) {
+      Print(L"failed to save memory map: %r\n", status);
+      Halt();
+    }
+    status = memmap_file->Close(memmap_file);
+    if (EFI_ERROR(status)) {
+      Print(L"failed to close memory map: %r\n", status);
+      Halt();
+    }
+  }
 
-  // 메모리 맵을 파일에 저장
-  SaveMemoryMap(&memmap, memmap_file);
-  memmap_file->Close(memmap_file);
-
-  //gop
   EFI_GRAPHICS_OUTPUT_PROTOCOL* gop;
-  OpenGOP(image_handle, &gop);
-  Print(L"Resolution: %ux%u, Pixel format: %s, %u pixels/line\n",
-    gop->Mode->Info->HorizontalResolution,
-    gop->Mode->Info->VerticalResolution,
-    GetPixelFormatUnicode(gop->Mode->Info->PixelFormat),
-    gop->Mode->Info->PixelsPerScanLine);
-  Print(L"Frame Buffer: 0x%01x - 0x%01x, Size: %lu bytes\n",
-    gop->Mode->FrameBufferBase,
-    gop->Mode->FrameBufferBase + gop->Mode->FrameBufferSize,
-    gop->Mode->FrameBufferSize);
-  
+  status = OpenGOP(image_handle, &gop);
+  if (EFI_ERROR(status)) {
+    Print(L"failed to open GOP: %r\n", status);
+    Halt();
+  }
+
+  Print(L"Resolution: %ux%u, Pixel Format: %s, %u pixels/line\n",
+      gop->Mode->Info->HorizontalResolution,
+      gop->Mode->Info->VerticalResolution,
+      GetPixelFormatUnicode(gop->Mode->Info->PixelFormat),
+      gop->Mode->Info->PixelsPerScanLine);
+  Print(L"Frame Buffer: 0x%0lx - 0x%0lx, Size: %lu bytes\n",
+      gop->Mode->FrameBufferBase,
+      gop->Mode->FrameBufferBase + gop->Mode->FrameBufferSize,
+      gop->Mode->FrameBufferSize);
+
   UINT8* frame_buffer = (UINT8*)gop->Mode->FrameBufferBase;
-  for(UINTN i=0; i < gop->Mode->FrameBufferSize; ++i){
+  for (UINTN i = 0; i < gop->Mode->FrameBufferSize; ++i) {
     frame_buffer[i] = 255;
   }
 
   EFI_FILE_PROTOCOL* kernel_file;
-  root_dir->Open(
-    root_dir, &kernel_file, L"\\kernel.elf", EFI_FILE_MODE_READ,0
-  );
+  status = root_dir->Open(
+      root_dir, &kernel_file, L"\\kernel.elf",
+      EFI_FILE_MODE_READ, 0);
+  if (EFI_ERROR(status)) {
+    Print(L"failed to open file '\\kernel.elf': %r\n", status);
+    Halt();
+  }
 
   UINTN file_info_size = sizeof(EFI_FILE_INFO) + sizeof(CHAR16) * 12;
   UINT8 file_info_buffer[file_info_size];
-  kernel_file->GetInfo(
-    kernel_file, &gEfiFileInfoGuid, &file_info_size, file_info_buffer
-  );
+  status = kernel_file->GetInfo(
+      kernel_file, &gEfiFileInfoGuid,
+      &file_info_size, file_info_buffer);
+  if (EFI_ERROR(status)) {
+    Print(L"failed to get file information: %r\n", status);
+    Halt();
+  }
 
   EFI_FILE_INFO* file_info = (EFI_FILE_INFO*)file_info_buffer;
   UINTN kernel_file_size = file_info->FileSize;
 
-  // gBS->AllocatePages 예외처리
+  // #@@range_begin(alloc_error)
   EFI_PHYSICAL_ADDRESS kernel_base_addr = 0x100000;
   status = gBS->AllocatePages(
-    AllocateAddress, EfiLoaderData,
-    (kernel_file_size + 0xfff) / 0x1000, &kernel_base_addr
-  );
-  if(EFI_ERROR(status)){
+      AllocateAddress, EfiLoaderData,
+      (kernel_file_size + 0xfff) / 0x1000, &kernel_base_addr);
+  if (EFI_ERROR(status)) {
     Print(L"failed to allocate pages: %r", status);
     Halt();
   }
-  kernel_file->Read(kernel_file, &kernel_file_size, (VOID*)kernel_base_addr);
-  Print(L"Kernel: 0x%0lx (%lu bytes)\n", kernel_base_addr, kernel_file_size); // Kernel: 0x100000 (9824 bytes)
+  // #@@range_end(alloc_error)
+  status = kernel_file->Read(kernel_file, &kernel_file_size, (VOID*)kernel_base_addr);
+  if (EFI_ERROR(status)) {
+    Print(L"error: %r", status);
+    Halt();
+  }
+  Print(L"Kernel: 0x%0lx (%lu bytes)\n", kernel_base_addr, kernel_file_size);
 
-  EFI_STATUS status;
+  // #@@range_begin(exit_bs)
   status = gBS->ExitBootServices(image_handle, memmap.map_key);
-  if (EFI_ERROR(status)){
+  if (EFI_ERROR(status)) {
     status = GetMemoryMap(&memmap);
-    if (EFI_ERROR(status)){
+    if (EFI_ERROR(status)) {
       Print(L"failed to get memory map: %r\n", status);
-      while(1);
+      Halt();
     }
     status = gBS->ExitBootServices(image_handle, memmap.map_key);
-    if(EFI_ERROR(status)){
+    if (EFI_ERROR(status)) {
       Print(L"Could not exit boot service: %r\n", status);
-      while(1);
+      Halt();
     }
   }
+  // #@@range_end(exit_bs)
 
   UINT64 entry_addr = *(UINT64*)(kernel_base_addr + 24);
 
-  typedef void EntryPointType(UINT64, UINT64);
+  // #@@range_begin(pass_frame_buffer_config)
+  struct FrameBufferConfig config = {
+    (UINT8*)gop->Mode->FrameBufferBase,
+    gop->Mode->Info->PixelsPerScanLine,
+    gop->Mode->Info->HorizontalResolution,
+    gop->Mode->Info->VerticalResolution,
+    0
+  };
+  switch (gop->Mode->Info->PixelFormat) {
+    case PixelRedGreenBlueReserved8BitPerColor:
+      config.pixel_format = kPixelRGBResv8BitPerColor;
+      break;
+    case PixelBlueGreenRedReserved8BitPerColor:
+      config.pixel_format = kPixelBGRResv8BitPerColor;
+      break;
+    default:
+      Print(L"Unimplemented pixel format: %d\n", gop->Mode->Info->PixelFormat);
+      Halt();
+  }
+
+  typedef void EntryPointType(const struct FrameBufferConfig*);
   EntryPointType* entry_point = (EntryPointType*)entry_addr;
-  entry_point(gop->Mode->FrameBufferBase, gop->Mode->FrameBufferSize);
+  entry_point(&config);
+  // #@@range_end(pass_frame_buffer_config)
 
   Print(L"All done\n");
 
-  while(1);
+  while (1);
   return EFI_SUCCESS;
 }
