7cd1a9bf026d8b3a35d7ca3ac7aa9bed29eb2c1a
sanso62 <jmd9762@gmail.com>
2023-10-17 13:51:39 +0900

    osbook_day04c

--- kernel/main.cpp
+++ kernel/main.cpp
@@ -13,10 +13,22 @@ struct PixelColor {
   uint8_t r, g, b;
 };
 
-/** WritePixel은 한 개의 점을 그린다.
- * @retval 0   성공
- * @retval 0이 아니면 실패
- */
+class PixelWriter{
+  public:
+    PixelWriter(const FrameBufferConfig& config) : config_{config}{
+    }
+    virtual ~PixelWriter() = default;
+    virtual void Write(int x, int y, const PixelColor& c) = 0;
+  
+  protected:
+    uint8_t* PixelAt(int x, int y){
+      return config_.frame_buffer + 4 * (config_.pixels_per_scan_line * y + x);
+    }
+  
+  private:
+    const FrameBufferConfig& config_;
+};
+
 int WritePixel(const FrameBufferConfig& config,
                int x, int y, const PixelColor& c) {
   const int pixel_position = config.pixels_per_scan_line * y + x;
@@ -35,19 +47,60 @@ int WritePixel(const FrameBufferConfig& config,
   }
   return 0;
 }
-// #@@range_end(write_pixel)
 
-// #@@range_begin(call_write_pixel)
+class RGBResv8BitColorPixelWriter: public PixelWriter{
+  public:
+    using PixelWriter::PixelWriter;
+
+    virtual void Write(int x, int y, const PixelColor& c) override{
+      auto p = PixelAt(x, y);
+      p[0] = c.r;
+      p[1] = c.g;
+      p[2] = c.b;
+    }
+};
+
+class BGRResv8BitColorPixelWriter: public PixelWriter{
+  public:
+    using PixelWriter::PixelWriter;
+
+    virtual void Write(int x, int y, const PixelColor& c) override{
+      auto p = PixelAt(x, y);
+      p[0] = c.b;
+      p[1] = c.g;
+      p[2] = c.r;
+    }
+};
+
+void* operator new(size_t size, void* buf){
+  return buf;
+}
+
+void operator delete(void* obj) noexcept{
+}
+
+char pixel_writer_buf[sizeof(RGBResv8BitColorPixelWriter)];
+PixelWriter* pixel_writer;
+
 extern "C" void KernelMain(const FrameBufferConfig& frame_buffer_config) {
-  for (int x = 0; x < frame_buffer_config.horizontal_resolution; ++x) {
-    for (int y = 0; y < frame_buffer_config.vertical_resolution; ++y) {
-      WritePixel(frame_buffer_config, x, y, {255, 255, 255});
+  switch(frame_buffer_config.pixel_format){
+    case kPixelRGBResv8BitPerColor:
+      pixel_writer = new(pixel_writer_buf)RGBResv8BitColorPixelWriter(frame_buffer_config);
+      break;
+    case kPixelBGRResv8BitPerColor:
+      pixel_writer = new(pixel_writer_buf)BGRResv8BitColorPixelWriter(frame_buffer_config);
+      break;
+  }
+
+  for(int x = 0; x < frame_buffer_config.horizontal_resolution; ++x){
+    for(int y = 0; y < frame_buffer_config.vertical_resolution; ++y){
+      pixel_writer->Write(x, y, {255, 255, 255});
     }
   }
-  for (int x = 0; x < 200; ++x) {
-    for (int y = 0; y < 100; ++y) {
-      WritePixel(frame_buffer_config, 100 + x, 100 + y, {0, 255, 0});
+  for(int x = 0; x < 200; ++x){
+    for(int y = 0; y < 100; ++y){
+      pixel_writer->Write(x, y, {0, 255, 0});
     }
   }
-  while (1) __asm__("hlt");
+  while(1) __asm__("hlt");
 }
